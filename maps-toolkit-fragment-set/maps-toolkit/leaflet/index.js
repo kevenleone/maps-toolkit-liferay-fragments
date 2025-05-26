5c6dd4c679e94863957fd761f21c1a8d4e3a4aeb
Keven Leone <keven.santos.sz@gmail.com>
2025-05-26 13:03:35 -0300

    Adding Custom TyleLayers for Leaflet

--- maps-toolkit-fragment-set/maps-toolkit/leaflet/index.js
+++ maps-toolkit-fragment-set/maps-toolkit/leaflet/index.js
@@ -1,11 +1,58 @@
 (function () {
     const {
+        customMapTyleLayer,
         latitude = 37.7749,
         longitude = -122.4194,
+        mapTileLayer,
         showMarker = true,
         showUserLocation = false,
         zoom = 13,
     } = configuration;
+
+    const tileLayers = {
+        default: {
+            url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            options: {
+                attribution: "&copy; OpenStreetMap contributors",
+            },
+        },
+        ["cartodb.light_all"]: {
+            url: "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+            options: {
+                attribution: "&copy; CartoDB, OpenStreetMap contributors",
+            },
+        },
+        ["stadia.alidadesatellite"]: {
+            url: "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
+            options: {
+                minZoom: 0,
+                maxZoom: 20,
+                attribution:
+                    '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                ext: "jpg",
+            },
+        },
+        ["stadia.alidadesmooth"]: {
+            url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}",
+            options: {
+                minZoom: 0,
+                maxZoom: 20,
+                attribution:
+                    '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                ext: "png",
+            },
+        },
+        ["stadia.outdoors"]: {
+            url: "https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}",
+            options: {
+                minZoom: 0,
+                maxZoom: 20,
+                attribution:
+                    '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                ext: "png",
+            },
+        },
+    };
 
     const leafletCSS = document.createElement("link");
     const leafletScript = document.createElement("script");
@@ -23,20 +70,13 @@
             zoom
         );
 
-        // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
-        //   map
-        // );
+        let tileLayer = tileLayers[mapTileLayer] || tileLayers.default;
 
-        // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        //   attribution: "&copy; OpenStreetMap contributors",
-        // }).addTo(map);
+        if (mapTileLayer === "other" && customMapTyleLayer) {
+            tileLayer = JSON.parse(customMapTyleLayer);
+        }
 
-        L.tileLayer(
-            "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
-            {
-                attribution: "&copy; CartoDB, OpenStreetMap contributors",
-            }
-        ).addTo(map);
+        L.tileLayer(tileLayer.url, tileLayer.options).addTo(map);
 
         if (showMarker) {
             L.marker([latitude, longitude])
